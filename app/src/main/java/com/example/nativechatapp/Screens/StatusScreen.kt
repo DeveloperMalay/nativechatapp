19e026db9b2067afd8a161c1ccb07112efc0daf3
Malay Pandit <malay.pandit@dinereel.com>
2024-03-31 11:30:19 +0530

    adding bottomnavigation bar to the screens

--- app/src/main/java/com/example/nativechatapp/Screens/StatusScreen.kt
+++ app/src/main/java/com/example/nativechatapp/Screens/StatusScreen.kt
@@ -1,9 +1,28 @@
 package com.example.nativechatapp.Screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.nativechatapp.LCViewModel
 
 @Composable
 fun StatusScreen(navController: NavController, vm: LCViewModel) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Status screen", modifier = Modifier.padding(8.dp))
+        BottomNavigationMenu(
+            selectedItem = BottomNavigationItem.STATUSLIST,
+            navController = navController
+        )
+    }
 }
