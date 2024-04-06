374ea4edc21fb6d98bab5e11574f95252c64a084
Malay Pandit <malay.pandit@dinereel.com>
2024-04-06 23:48:16 +0530

    started working on chat list screen

--- app/src/main/java/com/example/nativechatapp/Screens/ChatListScreen.kt
+++ app/src/main/java/com/example/nativechatapp/Screens/ChatListScreen.kt
@@ -4,12 +4,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.nativechatapp.CommonProgressBar
 import com.example.nativechatapp.LCViewModel
 
 @Composable
@@ -20,10 +22,23 @@ fun ChatListScreen(navController: NavController, vm: LCViewModel) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text("ChatList screen", modifier = Modifier.padding(8.dp))
+        val inProgress = vm.inProgress;
+        if (inProgress.value) {
+            CommonProgressBar()
+        } else {
+            Text(
+                "Chats", modifier = Modifier
+                    .padding(8.dp)
+            )
+        }
+
         BottomNavigationMenu(
             selectedItem = BottomNavigationItem.CHATLIST,
             navController = navController
         )
     }
+}
+
+fun FAB(showDialog: Boolean) {
+
 }
